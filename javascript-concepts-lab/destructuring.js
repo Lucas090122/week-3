07abbeb0907bcc34b6da0801782f87868b8cb0e2
Qingyun Wang <lucas.qingyun@gmail.com>
2024-09-02 13:15:25 +0300

    activity 1

---
+++ javascript-concepts-lab/destructuring.js
@@ -0,0 +1,8 @@
+const person = { name: 'Alice', age: 30, city: 'New York' };
+
+const { age, name } = person;
+const {city} = person;
+
+console.log('Name:', name);
+console.log('Age:', age);
+console.log('City', city);
